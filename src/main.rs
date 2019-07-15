c38cba3b539456a27140f1d3eeeba4b62e9ab71f
Jacob Riddle <ironlenny@gmail.com>
2019-07-15 15:14:20 -0700

    src/main.rs: Stub-out main
    Not ready to do anything in main yet.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use exitfailure::ExitFailure;
-use structopt::StructOpt;
+// use structopt::StructOpt;
 // use failure::ResultExt;
-use std::path::PathBuf;
+// use std::path::PathBuf;
 
 // Command-line struct
-#[derive(Debug, StructOpt)]
-#[structopt(about = "The Rust Parity archival program", author = "Jacob Riddle")]
-struct Args {
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
-}
+// #[derive(Debug, StructOpt)]
+// #[structopt(about = "The Rust Parity archival program", author = "Jacob Riddle")]
+// struct Args {
+//     #[structopt(parse(from_os_str))]
+//     input: PathBuf,
+// }
 
 fn main() -> Result<(), ExitFailure> {
-    let args = Args::from_args();
+    // let args = Args::from_args();
 
     Ok(())
 }
